aa857cdefafb75181a3d33991da8bdb9bdf11189
Omar Velazuez N <omi.velna.ov@gmail.com>
2026-05-23 23:29:44 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-aff9e039-17f6-4826-88f4-ec5e8528dff2.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
